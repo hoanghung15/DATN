3d69c994eaad9911c798e5e41735a1a47403feb0
hoanghung15 <hungaovai15122003@gmail.com>
2025-10-13 10:47:25 +0700

    test

--- DATN-BE/src/main/java/com/example/datnbe/base/config/SecurityConfig.java
+++ DATN-BE/src/main/java/com/example/datnbe/base/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-resources/**",
             "/webjars/**",
-            "/face/**"
+            "/face/**",
+            "/test/**"
     };
 
     @Bean
