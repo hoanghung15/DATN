dd62bf5b8ba973ab7884ad59207cc7d034576bac
hoanghung15 <hungaovai15122003@gmail.com>
2025-10-23 21:11:10 +0700

    add branch deploy

--- DATN-BE/src/main/java/com/example/datnbe/base/config/CorsConfig.java
+++ DATN-BE/src/main/java/com/example/datnbe/base/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://127.0.0.1:5500")
+                .allowedOriginPatterns("http://127.0.0.1:*","http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
